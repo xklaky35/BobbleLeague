492443fc4b578636c3862e603732ebfbe035d9b6
xklaky35 <xklaky@gmail.com>
2024-02-26 02:37:22 +0100

    arrow points to mouse

--- player.cs
+++ player.cs
@@ -1,41 +1,22 @@
 using Godot;
-using System;
 
 public partial class player : Node3D{
 
 	
-	RigidBody3D arrow;
-	RigidBody3D body;
 
-	Marker3D arrow_end;
-	Marker3D arrow_tip;
+    private const float RayRange = 10f;
 
 	[Export]
 	public int speed {get; set;}
 
+	public bool is_pressed;
 
-	public override void _Ready(){
-		arrow = GetNode<RigidBody3D>("Arrow");
-		body = GetNode<RigidBody3D>("Body");
-		arrow_end = GetNode<Marker3D>("Arrow/Arrow_end");
-		arrow_tip = GetNode<Marker3D>("Arrow/Arrow_tip");
-	}
+	public Vector3 direction;
 
-	public override void _PhysicsProcess(double delta){
-		arrow.Position = body.Position;
-	}
-	
-	private void _on_arrow_input_event(Node camera, InputEvent @event, Vector3 position, Vector3 normal, long shape_idx){
-		if(@event is InputEventMouseButton mouseButton && mouseButton.Pressed){
-			GD.Print("Mouse Pressed on arrow");
-		}
-	}
-
+    //add force to the 'Body element'
 	private void POWER(){
-
-		Vector3 new_dir =  arrow_tip.GlobalPosition - arrow_end.GlobalPosition;
-
-		body.ApplyCentralImpulse(new_dir * speed); 
+		var body = GetNode<RigidBody3D>("Body");
+		body.ApplyCentralImpulse(direction * speed);
 	}
 
 
@@ -43,9 +24,48 @@ public partial class player : Node3D{
 		POWER();
 	}
 
+	private void _on_body_input_event(Node camera, InputEvent @event, Vector3 position, Vector3 normal, long shape_idx){
+        
+        //click on the player body
+		if(@event is InputEventMouseButton mouseButton && mouseButton.Pressed){
+			GD.Print("Mouse Pressed on arrow");
+			is_pressed = true;
+		}
+	}
+
+    // Handle overall input 
+	public override void _Input(InputEvent @event){
+
+        //mouse button released
+		if(@event is InputEventMouseButton mouseButton && mouseButton.Pressed == false){
+			is_pressed = false;
+		}
+
+        //mouse moved while body clicked
+        if(@event is InputEventMouseMotion mouseMotion){
+            if(is_pressed){
+                var camera = GetNode<Camera3D>("../Camera3D");
+
+                Vector3 origin = camera.ProjectRayOrigin(mouseMotion.Position);
+                GD.Print(origin);
+
+                Vector3 target = origin + camera.ProjectRayNormal(mouseMotion.Position)*RayRange; 
+                GD.Print(target);
+                
+                var query = PhysicsRayQueryParameters3D.Create(origin, target);
+
+                var intersection = GetWorld3D().DirectSpaceState.IntersectRay(query);
+                var exact_looking_pos = (Vector3)intersection["position"];
+
+                Vector3 looking_pos = new Vector3(exact_looking_pos.X, Position.Y, exact_looking_pos.Z);
+                
+                Vector3 up_vector = new Vector3(0,1,0);
+
+                GetNode<MeshInstance3D>("Arrow").LookAt(looking_pos,up_vector);
+
+
+            }
+        }
+	}
 }
-
-
-
-
 
